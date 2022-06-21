4803c731b60f5665fd48beceb1e37295735347bb
skt-dev <98594874+skt-dev@users.noreply.github.com>
2022-06-21 12:15:11 +0530

    code optimization

--- script/main.js
+++ script/main.js
@@ -1,65 +1,90 @@
 
 const collections_url = "./mock/collections.json";
 const articles_urls = "./mock/articles.json";
-var collectionsData;
-async function getData() {
-    // Fetch API Data
-    const articlesResult = fetch(`${collections_url}`, { method: 'get' }) // FIRST API CALL
-        .then(response => response.json()) // pass the response as promise to next then block
-        .then(data => {                    // applpy logic to fetch collection type landing
-            collectionsData = data.Collections.find(o => o.collectiontype === 'landing')
-            const collectionId = collectionsData.collectionid;
-            return fetch(`${articles_urls}#${collectionId}`); // SECOND API CALL to fetch articles and return a promise
-        })
-        .then(function (response) {
-            if (response) {
-                hideloader();   // hide loader text container
+let collectionsData = "";
+
+const getCollections = async () => {
+    // Fetch Collections Data
+    return fetch(`${collections_url}`, { method: 'get' }) // FIRST API CALL
+        .then(response => {
+            if (response.ok) {          // returns true if the response returned
+                return response.json(); // pass the response as promise
+            } else {                    // Failed
+                setUserMessage("Some error occured");
             }
-            return response.json()
+        }) // pass the response as promise
+        .catch(err => {
+            console.error('Request failed', err);
+        })
+}
+const getArticles = async (collectionId) => {
+    // Fetch Articles Data
+    return fetch(`${articles_urls}#${collectionId}`) // SECOND API CALL to fetch articles and return a promise
+        .then((response) => {
+            if (response.ok) {          // returns true if the response returned
+                return response.json(); // pass the response as promise
+            } else {                    // Failed
+                setUserMessage("Some error occured");
+            }
         })
         .catch(err => {
-            console.error('Request failed', err)
+            console.error('Request failed', err);
         })
-
-    articlesResult.then(ret => {
-        const data = ret[collectionsData.collectionid]; // Get articles of elected collectionid
-        show(data);                                     // render data in UI
-    });
 }
 
-getData(); // Fetch data
+const collectionsPromise = getCollections(); // Fetch data
+collectionsPromise.then(data => {                    // applpy logic to fetch collection type landing
+    if (data && data.Collections) {
+        collectionsData = data.Collections.find(o => o.collectiontype === 'landing');
+        const collectionId = collectionsData.collectionid;
+        const articlesPromise = getArticles(collectionId); // Fetch data
+        articlesPromise.then(ret => {
+            if (ret) {
+                const data = (collectionsData && collectionsData.collectionid) ? ret[collectionsData.collectionid] : ''; // Get articles of elected collectionid
+                show(data);     // render data in UI
+                hideloader();   // hide loader text container
+            } else {
+                setUserMessage("Some error occured");
+            }
+        });
+    }
+});
 
-function hideloader() { // hide loader text container
-    document.getElementById('loading').style.display = 'none';
-}
+// hide loader text container
+const hideloader = () => document.getElementById('loading').style.display = 'none';
 
-function show(articlesData) {
+const setUserMessage = (message) => document.getElementById('loading').innerHTML = message;
+
+const show = (articlesData) => {
     // Process article data and render to UI.
-    const articles = articlesData.map((article, index) => {
-        return `<div class="article-wrapper article-col article-${ index }-wrapper">
-                        <div class="article-content ${ article.intro ? '' : 'article-empty-content' }">
-                            <div class="article-media">
-                                <img src="${ article.imageURL }" />
-                            </div>
-                            <div class="article-text">
-                                <div class="heading">
-                                    <span class="publiched-clock icon icon-title"></span>
-                                    ${ article.title }
+    let article = `<div class="article-wrapper article-col article-0-wrapper">No Data</div>`
+    if (articlesData) {
+        articles = articlesData.map((article, index) => {
+            return `<div class="article-wrapper article-col article-${index}-wrapper">
+                            <div class="article-content ${article.content ? '' : 'article-empty-content'}">
+                                <div class="article-media">
+                                    <img src="${article.imageURL}" />
                                 </div>
-                                <div class="content">
-                                <span class="intro-text caps-on ${ index % 2 == 0 ? 'odd-intro' : 'even-intro' }"">${ article.intro }</span>
-                                <span class="main-text">${ article.content }</span>
+                                <div class="article-text">
+                                    <div class="heading">
+                                        <span class="publiched-clock icon icon-title"></span>
+                                        ${article.title}
+                                    </div>
+                                    <div class="content">
+                                    <span class="intro-text caps-on ${article.intro ? (index == 0 ? 'red-intro' : 'blue-intro') : ''}"">${article.intro}</span>
+                                    <span class="main-text">${article.content}</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="article-footer">
-                            <span class="publiched-clock icon icon-clock"></span>
-                            <span class="published-timer">${ article.published }h</span>
-                            <span class="published-comments icon icon-speach-bubble"></cpan>
-                        </div>
-                        <div class="divider-line"></div>
-                        </div>`
-    }).join('');
+                            <div class="article-footer">
+                                <span class="publiched-clock icon icon-clock"></span>
+                                <span class="published-timer">${article.published}h</span>
+                                <span class="published-comments icon icon-speach-bubble"></cpan>
+                            </div>
+                            <div class="divider-line"></div>
+                            </div>`
+        }).join('');
+    }
 
-    document.getElementById("content_wrapper").innerHTML = articles;    // insert data to content wrapper
+    document.getElementById("content_wrapper").innerHTML = articles;    // append data to content wrapper
 }
